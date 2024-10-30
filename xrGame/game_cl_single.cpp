ada4a33f4d3b679959f3c74d00e8d7dcb5bb1f12
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-10-31 02:17:20 +0400

    Фикс легендарного уровня сложности

--- xrGame/game_cl_single.cpp
+++ xrGame/game_cl_single.cpp
@@ -39,5 +39,6 @@ void CScriptGameDifficulty::script_register(lua_State* L)
 {
 	module(L)[class_<enum_exporter<ESingleGameDifficulty>>("game_difficulty")
 				  .enum_("game_difficulty")[value("novice", int(egdNovice)), value("stalker", int(egdStalker)),
-											value("veteran", int(egdVeteran)), value("master", int(egdMaster))]];
+											value("veteran", int(egdVeteran)), value("master", int(egdMaster)),
+											value("legend", int(egdLegend))]];
 }
