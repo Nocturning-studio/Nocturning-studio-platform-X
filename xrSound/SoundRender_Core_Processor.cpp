0988a25d9ed7d508c5af92612510b66493ddb23f
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-12-03 17:38:49 +0400

    Исправление предупреждений xrSound

--- xrSound/SoundRender_Core_Processor.cpp
+++ xrSound/SoundRender_Core_Processor.cpp
@@ -264,16 +264,16 @@ float CSoundRender_Core::get_occlusion(Fvector& P, float R, Fvector* occ)
 			if (0 != ETOOLS::r_count())
 			{
 				// cache polygon
-				const CDB::RESULT* R = ETOOLS::r_begin();
+				const CDB::RESULT* Result = ETOOLS::r_begin();
 #else
 			geom_DB.ray_options(CDB::OPT_ONLYNEAREST);
 			geom_DB.ray_query(geom_MODEL, base, dir, range);
 			if (0 != geom_DB.r_count())
 			{
 				// cache polygon
-				const CDB::RESULT* R = geom_DB.r_begin();
+				const CDB::RESULT* Result = geom_DB.r_begin();
 #endif
-				const CDB::TRI& T = geom_MODEL->get_tris()[R->id];
+				const CDB::TRI& T = geom_MODEL->get_tris()[Result->id];
 				const Fvector* V = geom_MODEL->get_verts();
 				occ[0].set(V[T.verts[0]]);
 				occ[1].set(V[T.verts[1]]);
@@ -299,8 +299,8 @@ float CSoundRender_Core::get_occlusion(Fvector& P, float R, Fvector* occ)
 		{
 			for (u32 k = 0; k < r_cnt; k++)
 			{
-				CDB::RESULT* R = _B + k;
-				occ_value *= *(float*)&R->dummy;
+				CDB::RESULT* Result = _B + k;
+				occ_value *= *(float*)&Result->dummy;
 			}
 		}
 	}
