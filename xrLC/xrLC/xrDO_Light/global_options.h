1508dbb017d033e4462be7d28878aecee63bf9a3
NSDeathman <Forest.Kir.325@gmail.com>
2024-12-19 02:25:30 +0400

    Удалены редакторы

--- xrLC/xrLC/xrDO_Light/global_options.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern BOOL						b_norgb;
-extern BOOL						b_nosun;
-	   int						i_thread_count = 16;
--- xrLC/xrShared/lzhuf.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-extern XRCORE_API void		_compressLZ		(u8** dest, unsigned* dest_sz, void* src, unsigned src_sz);
-extern XRCORE_API void		_decompressLZ	(u8** dest, unsigned* dest_sz, void* src, unsigned src_sz);
