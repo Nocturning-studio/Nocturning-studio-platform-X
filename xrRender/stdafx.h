df832032c61016ddb5ef6d1ae564d9daf30a7d3b
NSDeathman <Forest.Kir.325@gmail.com>
2024-12-20 17:43:07 +0400

    Подключение d3dx9.lib перенесено в stdafx

--- xrRender/stdafx.h
+++ xrRender/stdafx.h
@@ -26,6 +26,8 @@
 #include "render.h"
 #include "r_color_converting.h"
 
+#pragma comment(lib, "d3dx9.lib")
+
 IC	void	jitter(CBlender_Compile& C)
 {
 	//C.r_Sampler	("jitter0",	JITTER(0), true, D3DTADDRESS_WRAP, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
