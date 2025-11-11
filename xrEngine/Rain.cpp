723c65612d95108f9fe534bdf37a2bf34612afa7
mihan-323 <81863440+mihan-323@users.noreply.github.com>
2025-11-11 21:34:43 +0400

    Кулинг для дождя

--- xrEngine/Rain.cpp
+++ xrEngine/Rain.cpp
@@ -319,14 +319,14 @@ void CEffect_Rain::Render()
 	// Render if needed
 	if (vCount)
 	{
-#pragma message(Reminder("Not implemented!"))
 		// HW.pDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+		StateManager.SetCullMode(D3D11_CULL_NONE);
 		RCache.set_xform_world(Fidentity);
 		RCache.set_Shader(SH_Rain);
 		RCache.set_Geometry(hGeom_Rain);
 		RCache.Render(D3DPT_TRIANGLELIST, vOffset, 0, vCount, 0, vCount / 2);
-#pragma message(Reminder("Not implemented!"))
 		// HW.pDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+		StateManager.SetCullMode(D3D11_CULL_CCW);
 	}
 
 	// Particles
